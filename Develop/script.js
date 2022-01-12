a4badd37672a5865e3754c9ff0cfd4ffe34ba1ee
codenamehemingway <jared.hemingway.jh@gmail.com>
2022-01-12 14:56:26 -0700

    added PW variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,10 @@
 // Assignment code here
 
+// setting variables
+var abcUpper = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+var abcLower = "abcdefghijklmnopqrstuvwxyz";
+var num = "0123456789";
+var sym = "!#$%&()*+,-./:;<=>?@^[]^_`{|}~";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +15,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
